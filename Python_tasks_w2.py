b0d8f49f2db0f3e021ee726206677fa77443b09a
alisalmans <alisalman2688@gmail.com>
2025-08-05 14:45:39 +0500

    second commit

---
+++ Python_tasks_w2.py
@@ -0,0 +1,12 @@
+cities=["rwp","isb",8,"haha"]
+# print(cities)
+# print(cities[2])
+# print(cities[-1])
+# print(cities[-3])
+# print(cities[1:3])
+
+cities.append("naenaaaaaa")
+cities.remove("naenaaaaaa")
+cities.insert(1,"lhr")
+cities.sort()
+print(cities)  
---
+++ new.py
@@ -0,0 +1,6 @@
+print("hello world")
+a=10
+if a>3:
+    print("yes")
+else:
+    print("no")
